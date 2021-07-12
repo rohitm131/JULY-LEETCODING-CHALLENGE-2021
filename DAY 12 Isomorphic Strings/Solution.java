aecaf760b281f8e3ccf1636c93e5cb496ad0f9f4
Rohit Mittal <43905130+rohitm131@users.noreply.github.com>
2021-07-12 18:59:51 +0530

    Create Solution.java

---
+++ DAY 12 Isomorphic Strings/Solution.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        
+        if(s.length() != t.length()) return false;
+        
+        Map<Character, Character> sTMap = new HashMap<>();
+        Map<Character, Character> tSMap = new HashMap<>();
+        
+        for(int i=0; i<s.length(); i++) {
+            char sChar = s.charAt(i);
+            char tChar = t.charAt(i);
+            
+            if(sTMap.containsKey(sChar)) {
+                if(sTMap.get(sChar) != tChar) return false;
+            }
+            
+            if(tSMap.containsKey(tChar)) {
+                if(tSMap.get(tChar) != sChar) return false;
+            }
+            
+            sTMap.put(sChar, tChar);
+            tSMap.put(tChar, sChar);
+        }
+        
+        return true;
+        
+    }
+}
